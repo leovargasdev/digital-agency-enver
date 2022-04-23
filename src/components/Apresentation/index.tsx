59597897f05096030b294a37ae409f25cca9ca0d
Leonardo Vargas <leu1607@hotmail.com>
2022-04-23 15:02:09 -0300

    feat: adicionando a seção Apresentation

---
+++ src/components/Apresentation/index.tsx
@@ -0,0 +1,22 @@
+import { IconPlay } from 'assets'
+import styles from './styles.module.scss'
+
+export const Apresentation = () => (
+  <section className={styles.container}>
+    <div>
+      <div className={styles.text}>
+        <h1>Why Enver Is The Best Choice?</h1>
+        <p>
+          Watch this one minute video so you understand why you should use our
+          services!
+        </p>
+      </div>
+
+      <div className={styles.video}>
+        <button type="button">
+          <IconPlay />
+        </button>
+      </div>
+    </div>
+  </section>
+)
