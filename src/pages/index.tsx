235730021fc7a8dad31749cd104b5e39ca8d5bb4
Leonardo Vargas <leu1607@hotmail.com>
2022-04-23 14:33:24 -0300

    feat: adicionando os componentes Header e BannerHero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,43 @@
-import { IconCircles } from 'assets'
 import Head from 'next/head'
 
-// import styles from 'styles/home.module.scss'
+import { Header } from 'components/Header'
+import { BannerHero } from 'components/BannerHero'
+
+import styles from 'styles/home.module.scss'
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Digital Agency Enver</title>
       </Head>
 
-      <h1>
-        olar <IconCircles />
-      </h1>
-      <h2>
-        olar <IconCircles />
-      </h2>
-    </>
+      <Header />
+      <BannerHero />
+
+      {/* <header>
+        <div>
+          <h1>header</h1>
+        </div>
+      </header>
+
+      <section>
+        <div>
+          <h1>section 1</h1>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>section 2</h1>
+        </div>
+      </section>
+
+      <footer>
+        <div>
+          <h1>footer 2</h1>
+        </div>
+      </footer> */}
+    </div>
   )
 }
